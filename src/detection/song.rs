fa5203013c369ae10696c26552d4ef63bf3e558b
phoenix <kundeng00@pm.me>
2025-10-21 20:02:19 -0400

    tsk-45: Adding functionality for itdentifying Song

---
+++ src/detection/song.rs
@@ -0,0 +1,15 @@
+
+
+pub fn file_type_from_filepath(filepath: &str) -> Result<String, std::io::Error> {
+    match infer::get_from_path(filepath) {
+        Ok(Some(kind)) => {
+            Ok(String::new())
+        }
+        Ok(None) => {
+            Err(std::io::Error::other("File type not determined"))
+        }
+        Err(err) => {
+            Err(err)
+        }
+    }
+}
