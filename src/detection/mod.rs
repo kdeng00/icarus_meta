f5e6ac567e199f302b9523eaf03dafc8ad92d7b1
phoenix <kundeng00@pm.me>
2025-11-06 15:52:46 +0000

    tsk-49: Initialize FileType with file type of the media (#51)
    Closes #49
    
    Reviewed-on: https://git.kundeng.us/phoenix/icarus_meta/pulls/51
    Co-authored-by: phoenix <kundeng00@pm.me>
    Co-committed-by: phoenix <kundeng00@pm.me>

--- src/detection/mod.rs
+++ src/detection/mod.rs
@@ -6,3 +6,29 @@ pub struct FileType {
     pub mime: String,
     pub file_type: String,
 }
+
+/// Initializes a FileType given a filetype passed.
+pub fn init_filetype(filetype: &str) -> Result<FileType, std::io::Error> {
+    if filetype == song::constants::FLAC_TYPE {
+        Ok(FileType {
+            mime: String::from(song::constants::mime::FLAC),
+            file_type: String::from(song::constants::FLAC_TYPE),
+        })
+    } else if filetype == coverart::constants::PNG_TYPE {
+        Ok(FileType {
+            mime: String::from(coverart::constants::mime::PNG),
+            file_type: String::from(coverart::constants::PNG_TYPE),
+        })
+    } else if filetype == coverart::constants::JPEG_TYPE
+        || filetype == coverart::constants::JPG_TYPE
+    {
+        Ok(FileType {
+            mime: String::from(coverart::constants::mime::JPEG),
+            file_type: String::from(coverart::constants::JPEG_TYPE),
+        })
+    } else {
+        Err(std::io::Error::other(format!(
+            "Unsupported FileType: {filetype:?}"
+        )))
+    }
+}
