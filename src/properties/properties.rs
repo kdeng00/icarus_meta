4459d89eabfb82fc4c6d5825477aa1d7ade56270
phoenix <kundeng00@pm.me>
2025-11-06 11:29:36 -0500

    tsk-50: Created properties module

--- src/properties/properties.rs
+++ src/properties/properties.rs
@@ -15,37 +15,3 @@ pub fn get_duration(song_path: &String) -> Result<std::time::Duration, std::io::
         Err(err) => Err(err),
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use crate::test_util;
-
-    #[test]
-    fn test_get_duration() {
-        let filename = test_util::util::get_filename(1);
-        let dir = String::from(test_util::util::TESTFILEDIRECTORY);
-
-        match test_util::util::file_exists(&dir, &filename) {
-            Ok(_) => {
-                let filepath = test_util::util::get_full_path(&dir, &filename).unwrap();
-                match super::get_duration(&filepath) {
-                    Ok(duration) => {
-                        let song_duration: u64 = 41;
-                        let fetched_song_duration = duration.as_secs();
-
-                        assert_eq!(
-                            song_duration, fetched_song_duration,
-                            "Durations should match, but they don't {song_duration} {fetched_song_duration} ({duration:?})"
-                        );
-                    }
-                    Err(err) => {
-                        assert!(false, "Error: {err:?}");
-                    }
-                }
-            }
-            Err(err) => {
-                assert!(false, "Error: {err:?}");
-            }
-        }
-    }
-}
